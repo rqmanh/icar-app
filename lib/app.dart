a89dea4641a8ee8930cbd9fdcdb91bd6ae4612fd
Mohammed Alsayani <mohammedalsayaniii@gmail.com>
2024-11-19 18:55:24 +0300

    refactor the app structure

--- lib/app.dart
+++ lib/app.dart
@@ -1,24 +1,34 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:icar/config/routre/app_routes.dart';
 import 'package:icar/config/theme/app_theme.dart';
-import 'package:icar/config/theme/widgets_theme.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
+  const designSize = Size(412, 892);
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-        title: 'iCar',
-        locale: context.locale,
-        supportedLocales: context.supportedLocales,
-        localizationsDelegates: context.localizationDelegates,
-        theme: TAppTheme.lightTheme,
-        routerConfig: router);
+    return ScreenUtilInit(
+      designSize: designSize,
+      fontSizeResolver: (fontSize, instance) {
+        final display = View.of(context).display;
+        final screenSize = display.size / display.devicePixelRatio;
+        final scaleWidth = screenSize.width / designSize.width;
+
+        return fontSize * scaleWidth;
+      },
+      child: MaterialApp.router(
+          title: 'iCar',
+          locale: context.locale,
+          supportedLocales: context.supportedLocales,
+          localizationsDelegates: context.localizationDelegates,
+          theme: TAppTheme.lightTheme,
+          routerConfig: router),
+    );
   }
 }
 
--- lib/config/app_keys.dart
+++ lib/config/app_keys.dart
@@ -1,25 +1,137 @@
 import 'package:easy_localization/easy_localization.dart';
 
 class AppKeys {
-  static String get authLogin => "auth.login".tr();
-  static String get authSignup => "auth.signup".tr();
-  static String get authLogout => "auth.logout".tr();
-  static String get authEmail => "auth.email".tr();
-  static String get authPassword => "auth.password".tr();
-  static String get authConfirmPassword => "auth.confirmPassword".tr();
-  static String get authForgotPassword => "auth.forgotPassword".tr();
-  static String get authResetPassword => "auth.resetPassword".tr();
-  static String get authSendCode => "auth.sendCode".tr();
-  static String get authVerificationCode => "auth.verificationCode".tr();
-  static String get authWelcomeBack => "auth.welcomeBack".tr();
-  static String get authPleaseLoginToContinue => "auth.pleaseLoginToContinue".tr();
-  static String get authEnterEmail => "auth.enterEmail".tr();
-  static String get authEnterPassword => "auth.enterPassword".tr();
-  static String get authEnterVerificationCode => "auth.enterVerificationCode".tr();
-  static String get authDontHaveAccount => "auth.dontHaveAccount".tr();
-  static String get authAlreadyHaveAccount => "auth.alreadyHaveAccount".tr();
-  static String get authCreateAccount => "auth.createAccount".tr();
-  static String get authInvalidEmail => "auth.invalidEmail".tr();
-  static String get authPasswordTooShort => "auth.passwordTooShort".tr();
-  static String get authPasswordMismatch => "auth.passwordMismatch".tr();
+  // Auth-related keys
+  static String welcome = _tr('welcome');
+  static String login = _tr('login');
+  static String signup = _tr('signup');
+  static String phoneNumber = _tr('phoneNumber');
+  static String password = _tr('password');
+  static String confirmPassword = _tr('confirmPassword');
+  static String fullName = _tr('fullName');
+  static String addPhotoOrAvatar = _tr('addPhotoOrAvatar');
+  static String getStarted = _tr('getStarted');
+  static String forgetPassword = _tr('forgetPassword');
+  static String signInWithGoogle = _tr('signInWithGoogle');
+  static String continueText = _tr('continue');
+
+  // Validation & Error-related keys
+  static String phoneRequired = _tr('phoneRequired');
+  static String phoneTooShort = _tr('phoneTooShort');
+  static String phoneTooLong = _tr('phoneTooLong');
+  static String phoneNoStart0 = _tr('phoneNoStart0');
+  static String phoneShouldStart5 = _tr('phoneShouldStart5');
+  static String phoneInvalid = _tr('phoneInvalid');
+  static String email = _tr('email');
+  static String emailCannotEmpty = _tr('emailCannotEmpty');
+  static String invalidEmailFormat = _tr('invalidEmailFormat');
+  static String shortPasswordError = _tr('shortPasswordError');
+  static String longPasswordError = _tr('longPasswordError');
+  static String passwordsDoesNotMatch = _tr('passwordsDoesNotMatch');
+  static String noRecordFound = _tr('noRecordFound');
+  static String requiredField = _tr('requiredField');
+  static String nameCannotEmpty = _tr('nameCannotEmpty');
+  static String phoneCannotEmpty = _tr('phoneCannotEmpty');
+  static String emailNotFound = _tr('emailNotFound');
+
+  static String invalidNameFormat = _tr('invalid_name_format');
+  static String invalidFormat = _tr('invalid_format');
+  static String invalidUsernameFormat = _tr('invalid_username_format');
+  static String shortUsernameError = _tr('short_username_error');
+  static String longUsernameError = _tr('long_username_error');
+  static String invalidCrNumberFormat = _tr('invalid_cr_number_format');
+  static String invalidPriceFormat = _tr('invalid_price_format');
+  static String invalidAddressFormat = _tr('invalid_address_format');
+
+  // OTP & Verification-related keys
+  static String otpTitle = _tr('otpTitle');
+  static String otpSubTitle = _tr('otpSubTitle');
+  static String otpMessage = _tr('otpMessage');
+  static String emailSent = _tr('emailSent');
+  static String congratulations = _tr('congratulations');
+  static String emailLinkToResetPassword = _tr('emailLinkToResetPassword');
+  static String accountCreateVerifyEmail = _tr('accountCreateVerifyEmail');
+  static String emailVerificationTitle = _tr('emailVerificationTitle');
+  static String emailVerificationSubTitle = _tr('emailVerificationSubTitle');
+  static String resendEmailLink = _tr('resendEmailLink');
+
+  // Profile-related keys
+  static String profile = _tr('profile');
+  static String editProfile = _tr('editProfile');
+  static String profileHeading = _tr('profileHeading');
+  static String profileSubHeading = _tr('profileSubHeading');
+
+  // Menu-related keys
+  static String logout = _tr('logout');
+  static String logoutDialogHeading = _tr('logoutDialogHeading');
+  static String menuLogout = _tr('menuLogout');
+  static String menuSettings = _tr('menuSettings');
+  static String menuInformation = _tr('menuInformation');
+  static String menuBillingDetails = _tr('menuBillingDetails');
+  static String menuUserManagement = _tr('menuUserManagement');
+  static String delete = _tr('delete');
+
+  // Miscellaneous keys
+  static String success = _tr('success');
+  static String notVerified = _tr('notVerified');
+  static String loading = _tr('loading');
+  static String nothingFound = _tr('nothingFound');
+  static String tryAgain = _tr('tryAgain');
+  static String thereIsSomethingError = _tr('thereIsSomethingError');
+  static String backToLogin = _tr('backToLogin');
+  static String joinAt = _tr('joinedAt');
+  static String joined = _tr('joined');
+
+  static String male = _tr('male');
+  static String female = _tr('female');
+  static String chooseGender = _tr('chooseGender');
+  static String submit = _tr('submit');
+  static String no = _tr('no');
+  static String yes = _tr('yes');
+  static String next = _tr('next');
+
+  static String resetPassword = _tr('resetPassword');
+  static String or = _tr('or');
+  static String connectWith = _tr('connectWith');
+  static String facebook = _tr('facebook');
+  static String google = _tr('google');
+  static String signUpTitle = _tr('signUpTitle');
+  static String signUpSubTitle = _tr('signUpSubTitle');
+  static String alreadyHaveAnAccount = _tr('alreadyHaveAnAccount');
+  static String forgetPasswordTitle = _tr('forgetPasswordTitle');
+  static String forgetPasswordSubTitle = _tr('forgetPasswordSubTitle');
+  static String resetViaEmail = _tr('resetViaEmail');
+  static String resetViaPhone = _tr('resetViaPhone');
+  static String forgetPhoneSubTitle = _tr('forgetPhoneSubTitle');
+  static String forgetMailSubTitle = _tr('forgetMailSubTitle');
+
+  static String welcomeBack = _tr('welcomeBack');
+  static String loginSubTitle = _tr('loginSubTitle');
+  static String rememberMe = _tr('rememberMe');
+  static String dontHaveAnAccount = _tr('dontHaveAnAccount');
+  static String enterYour = _tr('enterYour');
+
+  static String alert = _tr('alert');
+  static String ohSnap = _tr('ohSnap');
+
+  static String save = _tr('save');
+
+  static String justNow = _tr('justNow');
+  static String aMinuteAgo = _tr('aMinuteAgo');
+  static String anHourAgo = _tr('anHourAgo');
+  static String yesterday = _tr('yesterday');
+  static String week = _tr('week');
+  static String days = _tr('days');
+  static String day = _tr('day');
+  static String hours = _tr('hours');
+  static String hour = _tr('hour');
+  static String minutes = _tr('minutes');
+  static String minute = _tr('minute');
+  static String seconds = _tr('seconds');
+  static String selectDate = _tr('selectDate');
+
+    static String areYouSure = _tr('areYouSure');
+
+  // Helper method to fetch translation for keys
+  static String _tr(String key) => key.tr();
 }
